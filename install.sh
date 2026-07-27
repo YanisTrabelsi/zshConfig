68777d601be6a6bbca8ff1cf165ae9401660f855
YanisTrabelsi <yanis.trabelsi@epitech.digital>
2026-07-27 20:29:41 +0200

    test

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 FILE=.zshrc
-if [ -f "~/$FILE" ]; then
+if [ -f "~/$FILE" ]; then	
+	echo "==> Config founded"
 	mv "~/$FILE" "~/$FILE.bak"
+	echo "==> Config moved to ~/.$FILE.bak"
 fi
-mv $FILE ~/$FILE
+mv "$FILE" "~/$FILE"
 cd /
-rm -rf /zshConfig
+rm -rf "./zshConfig/"
